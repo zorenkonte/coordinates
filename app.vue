ff9ba07bedeb1650cffee900f08f9ea0d10109c6
Rnz Brngn <29701857+zorenkonte@users.noreply.github.com>
2025-01-02 17:01:07 +0800

    Create login page

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
+    <NuxtPage/>
   </div>
 </template>
---
+++ pages/login.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient()
+const email = ref('')
+const password = ref('')
+
+const signInWithPassword = async () => {
+  const { error } = await supabase.auth.signInWithPassword({
+    email: email.value,
+    password: password.value,
+  })
+  if (error) console.log(error)
+}
+</script>
+<template>
+  <div>
+    <button @click="signInWithPassword">
+      Sign In with E-Mail
+    </button>
+    <input
+      v-model="email"
+      type="email"
+    >
+    <input
+      v-model="password"
+      type="password"
+    >
+  </div>
+</template>
