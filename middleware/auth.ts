2a0bedec0c529cd7e82a18dd7879255194afc915
Rnz Brngn (zxreign) <29701857+zorenkonte@users.noreply.github.com>
2025-01-11 00:33:04 +0800

    Set user session after accepting invitation

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,16 @@
-export default defineNuxtRouteMiddleware((_, _from) => {
+export default defineNuxtRouteMiddleware(async (_, from) => {
   const user = useSupabaseUser()
+  const supabase = useSupabaseClient()
+
+  const access_token = from.hash.split('&').find(x => x.includes('access_token'))?.split('=')[1]
+  const refresh_token = from.hash.split('&').find(x => x.includes('refresh_token'))?.split('=')[1]
 
   if (user.value) {
     return navigateTo('/')
+  }
+  else if (access_token && refresh_token) {
+    await supabase.auth.setSession({ access_token, refresh_token })
+
+    window.location.href = '/'
   }
 })
