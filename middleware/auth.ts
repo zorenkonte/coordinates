5e98a821dbc7fca652fff5450626e97cf219e0c3
Rnz Brngn <29701857+zorenkonte@users.noreply.github.com>
2025-01-19 09:49:22 +0800

    Refactor auth check
    - Improve url parsing.
    - Use only the refresh token to authenticate user.

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,14 +2,14 @@ export default defineNuxtRouteMiddleware(async (_, from) => {
   const user = useSupabaseUser()
   const supabase = useSupabaseClient()
 
-  const access_token = from.hash.split('&').find(x => x.includes('access_token'))?.split('=')[1]
-  const refresh_token = from.hash.split('&').find(x => x.includes('refresh_token'))?.split('=')[1]
+  const urlParams = new URLSearchParams(from.hash.substring(1))
+  const refreshToken = urlParams.get('refresh_token')
 
   if (user.value) {
     return navigateTo('/')
   }
-  else if (access_token && refresh_token) {
-    await supabase.auth.setSession({ access_token, refresh_token })
+  else if (refreshToken) {
+    await supabase.auth.refreshSession({ refresh_token: refreshToken })
 
     window.location.href = '/'
   }
