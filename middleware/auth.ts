dd95edbb4bf4e4aaa16fc5eaa59f2fba7ab17905
Rnz Brngn (zxreign) <29701857+zorenkonte@users.noreply.github.com>
2025-01-20 20:47:28 +0800

    Manually set session for email invite auth

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -4,11 +4,12 @@ export default defineNuxtRouteMiddleware(async (_, from) => {
 
   const urlParams = new URLSearchParams(from.hash.substring(1))
   const refreshToken = urlParams.get('refresh_token')
+  const type = urlParams.get('type')
 
   if (user.value) {
     return navigateTo('/')
   }
-  else if (refreshToken) {
+  else if (refreshToken && type === 'invite') {
     await supabase.auth.refreshSession({ refresh_token: refreshToken })
 
     window.location.href = '/'
