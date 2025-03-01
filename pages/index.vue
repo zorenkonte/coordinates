ecf9528b2dd2f505fd67633c40050875ef8e2ce0
Rnz Brngn <29701857+zorenkonte@users.noreply.github.com>
2025-03-01 20:44:52 +0800

    Search by can

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@ const coordinates = ref<Tables<'coordinates'>[]>([])
 const limit = 10
 const offset = ref(0)
 
-const { data, refresh } = useAsyncData('coordinates', async () => {
+const { data } = useAsyncData('coordinates', async () => {
   const { data, error } = await client.from('coordinates').select().range(offset.value, offset.value + limit - 1)
   if (error || !data) {
     return []
@@ -21,20 +21,25 @@ onMounted(() => {
   }
 })
 
-async function loadCoordinates() {
-  await refresh()
-  if (data.value) {
-    coordinates.value = [...coordinates.value, ...data.value]
-    offset.value += limit
+const search = ref('')
+
+async function searchByCAN() {
+  const { data, error } = await client.from('coordinates').select().eq('can', search.value)
+  if (error || !data) {
+    return
   }
+  coordinates.value = data
 }
 </script>
 
 <template>
   <div class="max-w-sm mx-auto p-4">
+    <div class="flex items-center gap-2 mb-4">
+      <input v-model="search" type="text" placeholder="Search CAN" class="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring focus:ring-blue-500 focus:outline-none focus:border-blue-500">
+      <button class="px-4 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600" @click="searchByCAN">
+        Search
+      </button>
+    </div>
     <CoordinateList :coordinates="coordinates" />
-    <button class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600" @click="loadCoordinates">
-      Load More
-    </button>
   </div>
 </template>
