d770c3e743cad5d296c983ec7c5201b602e005bf
Rnz Brngn (zxreign) <29701857+zorenkonte@users.noreply.github.com>
2025-03-04 23:37:02 +0800

    Display loading indicator when searching

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { Search } from 'lucide-vue-next'
+import { Loader2, Search } from 'lucide-vue-next'
 
 const client = useSupabaseClient()
 
@@ -12,21 +12,32 @@ const { data } = useAsyncData('coordinates', async () => {
 
 const search = ref('')
 
-async function searchByCAN() {
-  const { data: searchData, error } = await client.from('coordinates').select().eq('can', search.value)
-  if (error || !searchData) {
-    return
-  }
-  data.value = searchData
-}
+const { status, execute } = useAsyncData(
+  'search-coordinates',
+  async () => {
+    const { data: searchData, error } = await client.from('coordinates').select().eq('can', search.value)
+    if (error || !searchData) {
+      return
+    }
+    data.value = searchData
+  },
+  { immediate: false },
+)
+
+const isSearching = computed(() => status.value === 'pending')
 </script>
 
 <template>
   <div class="max-w-sm mx-auto p-4">
     <div class="flex items-center gap-2 mb-4">
       <UiInput v-model="search" placeholder="Search CAN" />
-      <UiButton @click="searchByCAN">
-        <Search />
+      <UiButton :disabled="isSearching" @click="execute">
+        <template v-if="isSearching">
+          <Loader2 class="w-4 h-4 animate-spin" />
+        </template>
+        <template v-else>
+          <Search />
+        </template>
       </UiButton>
     </div>
     <CoordinateList :coordinates="data" />
