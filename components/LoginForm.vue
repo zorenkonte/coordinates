f2eaff2559a0087a7da3a1109d1307b0cbf1c5f0
Rnz Brngn (zxreign) <29701857+zorenkonte@users.noreply.github.com>
2025-03-13 23:09:04 +0800

    Use auto import component name

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
-import { Card, CardContent } from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import { Loader2 } from 'lucide-vue-next'
 
 const supabase = useSupabaseClient()
@@ -27,8 +23,8 @@ const isPending = computed(() => status.value === 'pending')
 
 <template>
   <div class="flex flex-col gap-6">
-    <Card class="overflow-hidden">
-      <CardContent class="grid p-0 md:grid-cols-2">
+    <UiCard class="overflow-hidden">
+      <UiCardContent class="grid p-0 md:grid-cols-2">
         <UiForm v-slot="{ handleSubmit }" as="div">
           <form class="p-6 md:p-8" @submit="handleSubmit(execute)">
             <div class="flex flex-col gap-6">
@@ -41,12 +37,16 @@ const isPending = computed(() => status.value === 'pending')
                 </p>
               </div>
               <div class="grid gap-2">
-                <Label for="email">{{ $t('email') }}</Label>
-                <Input id="email" v-model="email" type="email" placeholder="zxreign@bringino.com" required />
+                <UiLabel for="email">
+                  {{ $t('email') }}
+                </UiLabel>
+                <UiInput id="email" v-model="email" type="email" placeholder="zxreign@bringino.com" required />
               </div>
               <div class="grid gap-2">
                 <div class="flex items-center">
-                  <Label for="password">{{ $t('password') }}</Label>
+                  <UiLabel for="password">
+                    {{ $t('password') }}
+                  </UiLabel>
                   <a
                     href="#"
                     class="ml-auto text-sm underline-offset-2 hover:underline"
@@ -54,23 +54,23 @@ const isPending = computed(() => status.value === 'pending')
                     Forgot your password?
                   </a>
                 </div>
-                <Input id="password" v-model="password" type="password" required />
+                <UiInput id="password" v-model="password" type="password" required />
               </div>
-              <Button type="submit" class="w-full" :disabled="isPending">
+              <UiButton type="submit" class="w-full" :disabled="isPending">
                 <template v-if="isPending">
                   <Loader2 class="w-4 h-4 animate-spin" />
                 </template>
                 <template v-else>
                   {{ $t('login') }}
                 </template>
-              </Button>
+              </UiButton>
               <div class="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                 <span class="relative z-10 bg-background px-2 text-muted-foreground">
                   Or continue with
                 </span>
               </div>
               <div class="grid grid-cols-1">
-                <Button variant="outline" class="w-full">
+                <UiButton variant="outline" class="w-full">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
                       d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
@@ -78,7 +78,7 @@ const isPending = computed(() => status.value === 'pending')
                     />
                   </svg>
                   Login with Google
-                </Button>
+                </UiButton>
               </div>
               <div class="text-center text-sm">
                 Don&apos;t have an account?
@@ -96,8 +96,8 @@ const isPending = computed(() => status.value === 'pending')
             class="dark:brightness-[0.2] dark:grayscale"
           >
         </div>
-      </CardContent>
-    </Card>
+      </UiCardContent>
+    </UiCard>
     <div class="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
       By clicking continue, you agree to our <a href="#">Terms of Service</a>
       and <a href="#">Privacy Policy</a>.
